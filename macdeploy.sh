0452222efe7c30fede1d7c6a664a5c3193a5c952
Rainer Sieger <rsieger@pangaea.de>
2015-06-02 23:14:32 +0200

    macdeploy.sh - Qt 5.4.1 -> Qt 5.4.2

--- macdeploy.sh
+++ macdeploy.sh
@@ -7,7 +7,7 @@ echo - macdeployqt
 cd ~/Development/GitHub/PanPlot
 
 rm -R '../../Distribution/PanPlot2/PanPlot2.app'
-cp -R './build-PanPlot2-Desktop_Qt_5_4_1_clang_64bit-Release/PanPlot2.app' '../../Distribution/PanPlot2/PanPlot2.app'
+cp -R './build-PanPlot2-Desktop_Qt_5_4_2_clang_64bit-Release/PanPlot2.app' '../../Distribution/PanPlot2/PanPlot2.app'
 cp './trunk/Resources/Info.plist' '../../Distribution/PanPlot2/PanPlot2.app/Contents/Info.plist'
 
 /Developer/Qt/5.4/clang_64/bin/macdeployqt '../../Distribution/PanPlot2/PanPlot2.app'
